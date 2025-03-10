0ce29930f78212978c24418a0b7fb663f172c02f
mokuken <imsilencer44@gmail.com>
2025-03-10 23:07:47 +0800

    print function output directly to the console

--- Exercise/exercise_task2.py
+++ Exercise/exercise_task2.py
@@ -1,14 +1,7 @@
 math_students = {"Alice", "Bob", "Charlie", "David"}
 science_students = {"Charlie", "David", "Eve", "Frank"}
 
-all_students = math_students.union(science_students)
-print(all_students)
-
-same_student = math_students.intersection(science_students)
-print(same_student)
-
-not_math_student = math_students.difference(science_students)
-print(not_math_student)
-
-not_student = math_students.symmetric_difference(science_students)
-print(not_student)
+print(math_students.union(science_students))
+print(math_students.intersection(science_students))
+print(math_students.difference(science_students))
+print(math_students.symmetric_difference(science_students))
