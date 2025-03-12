bbf1e39c6815c2570a1513cfab9bd3b50bd16f91
mokuken <imsilencer44@gmail.com>
2025-03-12 08:31:49 +0800

    finnished the code

--- Activities/python_activity10.py
+++ Activities/python_activity10.py
@@ -1,63 +1,60 @@
 main_roads = ["Roxas Avenue", "Arnaldo Boulevard", "Fuentes Drive"]
 speed_limits = (80, 50, 60)
-vehicle_spotted = []
+vehicle_spotted = set()
 total_fines_per_road = {}
 
 vehicle_report = int(input("How many vehicle reports to process (1-5): "))
 
 for i in range(vehicle_report):
-    road_name = input("What is the road name: ")
+    road_name = input("\nWhat is the road name: ")
     license_plate = input("What is the license plate: ")
     speed = int(input("What is the speed: "))
 
-    vehicle_spotted.append(license_plate)
+    vehicle_spotted.add(license_plate)
 
+    fine = 0
     for road in main_roads:
         if road_name == road:
-            print(main_roads.index(road))
             if speed >= speed_limits[main_roads.index(road)]+10:
-                fine_count = 0
-                while speed > speed_limits[main_roads.index(road)]:
-                    fine_count+=1
-                    speed-=1
-                    fine = fine_count * 5
-
-                total_fines_per_road[road_name] = fine
-
-                print(fine)
-                print(total_fines_per_road)
+                fine = (speed - speed_limits[main_roads.index(road)]) * 5
             elif speed < 20:
                 print("The driver is a slow driver!")
 
+            total_fines_per_road[road_name] = fine
+
             break
-    else:
-        if road_name not in main_roads:
-            main_roads.append(road_name)
-            converted_tuple = list(speed_limits)
-            converted_tuple.append(70)
-            speed_limits = tuple(converted_tuple)
+        else:
+            if road_name not in main_roads:
+                main_roads.append(road_name)
+                converted_tuple = list(speed_limits)
+                converted_tuple.append(70)
+                speed_limits = tuple(converted_tuple)
 
-            total_fines_per_road[road_name] = 0
+                if speed >= 70:
+                    fine = (speed - 70) * 5
+                elif speed < 20:
+                    print("The driver is a slow driver!")
 
-            print(speed_limits)
+                total_fines_per_road[road_name] = fine
+    
+    print(f"Fine: {fine}")
     i += 1
 
-print(main_roads)
-high_fine = []
+total_fine = []
+for road, fine in total_fines_per_road.items():
+    total_fine.append(fine)
 
-for road, fines in total_fines_per_road.items():
-    high_fine.append(fines)
-
-
-if len(vehicle_spotted) < len(main_roads):
-    del fine.min()
-
-
-print(f"Active Roads: {main_roads}")
+print(f"\nActive Roads: {main_roads}")
 print(f"Speed Limits: {speed_limits}")
-print(f"Fine Totals: {sum(high_fine)}")
-print(f"Fine Totals: {high_fine}")
+print(f"Fine Totals: {sum(total_fine)}")
 
-print(high_fine)
+worst_road_fine = 0
+worst_road = ""
 
+for road_name, fine in total_fines_per_road.items():
+    if fine > worst_road_fine:
+        worst_road_fine = fine
+        worst_road = road_name
 
+print(f"Worst Road: {worst_road}")
+print(f"Unique Vehicles: {len(vehicle_spotted)}\n")
