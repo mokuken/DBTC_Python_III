73e91b9d1c6f58ee4a5cf67aec3402a2a0aba10f
mokuken <imsilencer44@gmail.com>
2025-04-03 11:43:49 +0800

    added new documentation/explaination comments

--- Activities/python_activity15.py
+++ Activities/python_activity15.py
@@ -1,18 +1,17 @@
 # Refactored code
-# This code follows the Single Responsibility Principle (SRP).
+
+# I refactored the Order class to remove the orderCalculator, InvoicePrinter, OrderRepository methods and move into different classes
+# to follow the Singe Responsible Principle (SRP)
 class Order:
     def __init__(self, order_id, items):
         self.order_id = order_id
         self.items = items
     
-    # I removed the orderCalculator, InvoicePrinter, OrderRepository methods and move into different classes
-
-# This class is responsible for calculating the total price of the order.
+# and i also remove the @staticmethod to orderCalculator, InvoicePrinter, OrderRepository classes because it is static in default and make the code cleaner
 class OrderCalculator:
     def calculate_total(order):
         return sum(price for _, price in order.items)
 
-# This class is responsible for printing the invoice.
 class InvoicePrinter:
     def print_invoice(order):
         print(f"Invoice for Order ID: {order.order_id}")
@@ -20,7 +19,6 @@ class InvoicePrinter:
             print(f"{item}: ${price}")
         print(f"Total: ${OrderCalculator.calculate_total(order)}")
 
-# This class is responsible for saving the order to a database.
 class OrderRepository:
     def save_to_database(order):
         print(f"Saving order {order.order_id} to database...")
